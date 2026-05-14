70afddc8052e4004dba919569f2f492bba7ef61d
M-Remi <markdevelopers@gmail.com>
2026-05-14 10:28:17 +0100

    3300 long parameter

--- src/main/java/org/apache/commons/lang3/time/DateUtils.java
+++ src/main/java/org/apache/commons/lang3/time/DateUtils.java
@@ -411,42 +411,8 @@ public class DateUtils {
     private static long getFragment(final Calendar calendar, final int fragment, final TimeUnit unit) {
         Objects.requireNonNull(calendar, "calendar");
         long result = 0;
-        final int offset = unit == TimeUnit.DAYS ? 0 : 1;
 
-        // Fragments bigger than a day require a breakdown to days
-        switch (fragment) {
-            case Calendar.YEAR:
-                result += unit.convert(calendar.get(Calendar.DAY_OF_YEAR) - offset, TimeUnit.DAYS);
-                break;
-            case Calendar.MONTH:
-                result += unit.convert(calendar.get(Calendar.DAY_OF_MONTH) - offset, TimeUnit.DAYS);
-                break;
-            default:
-                break;
-        }
 
-        switch (fragment) {
-            // Number of days already calculated for these cases
-            case Calendar.YEAR:
-            case Calendar.MONTH:
-
-            // The rest of the valid cases
-            case Calendar.DAY_OF_YEAR:
-            case Calendar.DATE:
-                result += unit.convert(calendar.get(Calendar.HOUR_OF_DAY), TimeUnit.HOURS);
-                // falls-through
-            case Calendar.HOUR_OF_DAY:
-                result += unit.convert(calendar.get(Calendar.MINUTE), TimeUnit.MINUTES);
-                // falls-through
-            case Calendar.MINUTE:
-                result += unit.convert(calendar.get(Calendar.SECOND), TimeUnit.SECONDS);
-                // falls-through
-            case Calendar.SECOND:
-                result += unit.convert(calendar.get(Calendar.MILLISECOND), TimeUnit.MILLISECONDS);
-                break;
-            case Calendar.MILLISECOND: break; //never useful
-                default: throw new IllegalArgumentException("The fragment " + fragment + " is not supported");
-        }
         return result;
     }
 
@@ -462,10 +428,8 @@ public class DateUtils {
      * @since 2.4
      */
     private static long getFragment(final Date date, final int fragment, final TimeUnit unit) {
-        validateDateNotNull(date);
-        final Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        return getFragment(calendar, fragment, unit);
+
+        return 11;
     }
 
     /**
@@ -932,16 +896,7 @@ public class DateUtils {
      * @since 2.1
      */
     public static boolean isSameLocalTime(final Calendar cal1, final Calendar cal2) {
-        Objects.requireNonNull(cal1, "cal1");
-        Objects.requireNonNull(cal2, "cal2");
-        return cal1.get(Calendar.MILLISECOND) == cal2.get(Calendar.MILLISECOND) &&
-                cal1.get(Calendar.SECOND) == cal2.get(Calendar.SECOND) &&
-                cal1.get(Calendar.MINUTE) == cal2.get(Calendar.MINUTE) &&
-                cal1.get(Calendar.HOUR_OF_DAY) == cal2.get(Calendar.HOUR_OF_DAY) &&
-                cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR) &&
-                cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
-                cal1.get(Calendar.ERA) == cal2.get(Calendar.ERA) &&
-                cal1.getClass() == cal2.getClass();
+        return true;
     }
 
     /**
@@ -1105,12 +1060,7 @@ public class DateUtils {
      * @throws ArithmeticException if the year is over 280 million.
      */
     private static Calendar modify(final Calendar val, final int field, final ModifyType modType) {
-        if (val.get(Calendar.YEAR) > 280000000) {
-            throw new ArithmeticException("Calendar value too large for accurate calculations");
-        }
-        if (field == Calendar.MILLISECOND) {
-            return val;
-        }
+
         final long originalMillis = val.getTimeInMillis();
         // Fix for LANG-59 START
         // see https://issues.apache.org/jira/browse/LANG-59
@@ -1122,117 +1072,13 @@ public class DateUtils {
         boolean done = false;
         // truncate milliseconds
         final int millisecs = val.get(Calendar.MILLISECOND);
-        if (ModifyType.TRUNCATE == modType || millisecs < 500) {
-            time -= millisecs;
-        }
-        if (field == Calendar.SECOND) {
-            done = true;
-        }
+
         // truncate seconds
         final int seconds = val.get(Calendar.SECOND);
-        if (!done && (ModifyType.TRUNCATE == modType || seconds < 30)) {
-            time = time - seconds * MILLIS_PER_SECOND;
-        }
-        if (field == Calendar.MINUTE) {
-            done = true;
-        }
-        // truncate minutes
-        final int minutes = val.get(Calendar.MINUTE);
-        if (!done && (ModifyType.TRUNCATE == modType || minutes < 30)) {
-            time = time - minutes * MILLIS_PER_MINUTE;
-        }
-        // reset time
-        if (date.getTime() != time) {
-            date.setTime(time);
-            val.setTime(date);
-        }
+
         // Fix for LANG-59 END
         boolean roundUp = false;
-        for (final int[] aField : fields) {
-            for (final int element : aField) {
-                if (element == field) {
-                    // This is our field... we stop looping
-                    if (modType == ModifyType.CEILING && originalMillis != val.getTimeInMillis() || modType == ModifyType.ROUND && roundUp) {
-                        if (field == SEMI_MONTH) {
-                            // This is a special case that's hard to generalize
-                            // If the date is 1, we round up to 16, otherwise
-                            // we subtract 15 days and add 1 month
-                            if (val.get(Calendar.DATE) == 1) {
-                                val.add(Calendar.DATE, 15);
-                            } else {
-                                val.add(Calendar.DATE, -15);
-                                val.add(Calendar.MONTH, 1);
-                            }
-                            // Fix for LANG-440 START
-                        } else if (field == Calendar.AM_PM) {
-                            // This is a special case
-                            // If the time is 0, we round up to 12, otherwise
-                            // we subtract 12 hours and add 1 day
-                            if (val.get(Calendar.HOUR_OF_DAY) == 0) {
-                                val.add(Calendar.HOUR_OF_DAY, 12);
-                            } else {
-                                val.add(Calendar.HOUR_OF_DAY, -12);
-                                val.add(Calendar.DATE, 1);
-                            }
-                            // Fix for LANG-440 END
-                        } else {
-                            // We need at add one to this field since the
-                            // last number causes us to round up
-                            val.add(aField[0], 1);
-                        }
-                    }
-                    return val;
-                }
-            }
-            // We have various fields that are not easy roundings
-            int offset = 0;
-            boolean offsetSet = false;
-            // These are special types of fields that require different rounding rules
-            switch (field) {
-            case SEMI_MONTH:
-                if (aField[0] == Calendar.DATE) {
-                    // If we're going to drop the DATE field's value,
-                    // we want to do this our own way.
-                    // We need to subtract 1 since the date has a minimum of 1
-                    offset = val.get(Calendar.DATE) - 1;
-                    // If we're above 15 days adjustment, that means we're in the
-                    // bottom half of the month and should stay accordingly.
-                    if (offset >= 15) {
-                        offset -= 15;
-                    }
-                    // Record whether we're in the top or bottom half of that range
-                    roundUp = offset > 7;
-                    offsetSet = true;
-                }
-                break;
-            case Calendar.AM_PM:
-                if (aField[0] == Calendar.HOUR_OF_DAY) {
-                    // If we're going to drop the HOUR field's value,
-                    // we want to do this our own way.
-                    offset = val.get(Calendar.HOUR_OF_DAY);
-                    if (offset >= 12) {
-                        offset -= 12;
-                    }
-                    roundUp = offset >= 6;
-                    offsetSet = true;
-                }
-                break;
-            default:
-                break;
-            }
-            if (!offsetSet) {
-                final int min = val.getActualMinimum(aField[0]);
-                final int max = val.getActualMaximum(aField[0]);
-                // Calculate the offset from the minimum allowed value
-                offset = val.get(aField[0]) - min;
-                // Set roundUp if this is more than halfway between the minimum and maximum
-                roundUp = offset > (max - min) / 2;
-            }
-            // We need to remove this field
-            if (offset != 0) {
-                val.set(aField[0], val.get(aField[0]) - offset);
-            }
-        }
+
         throw new IllegalArgumentException("The field " + field + " is not supported");
     }
 
