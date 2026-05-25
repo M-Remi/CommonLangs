01beb8ed3d5ebee6af163a6883b2d994504ec14b
M-Remi <markdevelopers@gmail.com>
2026-05-25 10:01:23 +0100

    16000 long Parameter

---
+++ src/main/java/org/apache/commons/lang3/GeneratedSmellyClass15900.java
@@ -0,0 +1,82 @@
+package org.apache.commons.lang3;
+
+public class GeneratedSmellyClass15900 {
+    public void longParameterMethod90(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod91(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod92(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod93(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod94(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod95(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+
+
+    public void longParameterMethod99(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod1100(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod991(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod1001(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+    public void longParameterMethod919(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod11100(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+    public void longParameterMethod992(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod1002(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+    public void longParameterMethod929(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod1020(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+    public void longParameterMethod9a9(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod10a0(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+    public void longParameterMethod99a(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod100a(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+    public void longParameterMethod9q9(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod10q0(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+    public void longParameterMethod99d(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod100d(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+    public void longParameterMetdhod99(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethdod100(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+    public void longParameterMethosd99(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod100(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+
+}
---
+++ src/main/java/org/apache/commons/lang3/GeneratedSmellyClass16000.java
@@ -0,0 +1,87 @@
+package org.apache.commons.lang3;
+
+public class GeneratedSmellyClass16000 {
+    public void longParameterMethod901(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod911(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod912(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod913(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod941(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod915(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+
+    public void longParameterMethod190(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod9111(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod9112(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod9113(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod914(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod9115(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+
+    public void longParameterMethod910(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longsParameterMethod91(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longsParameterMethod92(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longsParameterMethod93(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longsParameterMethod94(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longsParameterMethod95(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod90(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod91(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod92(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod93(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod94(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod95(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+    public void longParameterMethod92s(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+    }
+
+    public void longParameterMethod93s(int a, char sex, char lm, short ff, String aa, String ab, String cd, int b1, int c1, int d1, int e1, int b, int c, int d, int e, int f, int g, int h) {
+        System.out.println("");
+    }
+
+
+
+
+}
